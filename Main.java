ec9dad543a48c97a110016e5bc3437fb8f5f6728
balez4464 <balez4464@wrdsb.ca>
2020-10-13 16:25:21 +0000

    I programmed a program that finds the first digit of a number using a method

--- Main.java
+++ Main.java
@@ -1,15 +1,37 @@
 /**
- *
- * @author 
+ *This program out prints the first digit of a number using a method
+ * @author Zachary Balean
  */
 public class Main {
 
+  //method for first digit
+  public static int firstDigit(int number){
+    //if statemetn if the number is negative, makes it positive
+    if(number < 0){
+      number = number * (-1);
+    }
+    //for loop for dividing by ten until the last digit
+    for(int i = number; number > 9; i++){
+      //divides number by 10
+      number = number / 10;
+    }
+
+    //integer for first digit
+    int firstNumber = number  % 10;
+
+    //returns the integer
+    return firstNumber;
+  }
+  
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // calls the method
+    int answer = firstDigit(3572);
+    //prints the last digit
+    System.out.println(answer);
+  
   }
 }
